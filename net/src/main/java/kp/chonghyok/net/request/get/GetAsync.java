9a7e382b13322c8e64d9aba0865f72fb348eba2d
richonghyok <richonghyok@qq.com>
2021-05-06 12:19:30 +0800

    net

--- net/src/main/java/kp/chonghyok/net/request/get/GetAsync.java
+++ net/src/main/java/kp/chonghyok/net/request/get/GetAsync.java
@@ -1,5 +1,7 @@
 package kp.chonghyok.net.request.get;
 
+import android.util.Log;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -8,9 +10,11 @@ import java.lang.reflect.Type;
 import kp.chonghyok.net.callback.ArrayCallback;
 import kp.chonghyok.net.callback.ObjCallback;
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import static kp.chonghyok.net.cookie.LocalCookieJar.getCookieJar;
 
@@ -51,6 +55,41 @@ public class GetAsync {
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) {
                 callback.onProcess(response, typeOfT);
+            }
+        });
+    }
+
+    public static void getAsync(String url, ObjCallback<String> callback) {
+        Log.d("getSync", url);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .cookieJar(getCookieJar())
+                .followRedirects(true)
+                .followSslRedirects(true)
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                callback.onFailure();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ResponseBody body = response.body();
+                    if (body != null) {
+                        String html = body.string();
+                        callback.onSuccess(html);
+                    }else{
+                        callback.onFailure();
+                    }
+                } else {
+                    callback.onFailure();
+                }
             }
         });
     }
